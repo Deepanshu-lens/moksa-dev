962e09e3f3338ef1273822bf7ab4eab0d3514886
deepanshu <deepanshu.khorwal@lenscorp.ai>
2024-11-21 17:18:34 +0530

    placeholder changed

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -88,7 +88,7 @@
           autoComplete="off"
           id="email"
           type="email"
-          placeholder="Email"
+          placeholder="please provide your email"
           name="email"
           required
         />
