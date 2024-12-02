3e93c39b5ba8102bef221c7bc5c44d91adfc3d2c
Kaustav-lens <kaustav.bhattacharya@lenscorp.ai>
2024-12-02 19:47:32 +0530

    chore: initial layout 1

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -3,5 +3,9 @@ import { writable } from "svelte/store";
 
 export const isAlertPanelOpen = writable<boolean>(false);
 export const isAlertMobileOpen = writable<boolean>(false);
-export const selectedLayout = writable<number>(typeof window !== 'undefined' ? Number(localStorage.getItem("selectedLayout")) || 5 : 5);
+export const selectedLayout = writable<number>(
+  typeof window !== "undefined"
+    ? Number(localStorage.getItem("selectedLayout")) || 1
+    : 1
+);
 export const isRoiPanelOpen = writable<boolean>(false);
