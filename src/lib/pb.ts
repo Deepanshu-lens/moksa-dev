e125b13f8ec81c95952c223f47300c1788a1310b
Kaustav-lens <kaustav.bhattacharya@lenscorp.ai>
2024-12-09 15:09:50 +0530

    chore: dynamic db url

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -1,5 +1,13 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE_URL);
-
-export default pb;
+const getPocketBaseURL = () => {
+    if (process.env.VERCEL || process.env.NODE_ENV === "production") {
+      return import.meta.env.PUBLIC_POCKETBASE_URL;
+    } else {
+      const localIP = window.location.hostname;
+      return `http://${localIP}:8090`;
+    }
+  };
+  const pb = new PocketBase(getPocketBaseURL());
+  
+  export default pb;
