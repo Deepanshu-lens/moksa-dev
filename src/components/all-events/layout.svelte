51906820198ab43af3ec2b789b9b457b0e694c93
Kaustav-lens <kaustav.bhattacharya@lenscorp.ai>
2024-12-30 12:04:38 +0530

    feat: events filter

--- src/components/all-events/layout.svelte
+++ src/components/all-events/layout.svelte
@@ -1,189 +1,177 @@
 <script lang="ts">
   import EventGrid from "./component/event-grid.svelte";
   import NodeSelection from "../node/NodeSelection.svelte";
+  import Button from "../ui/button/button.svelte";
+  import { Filter, X } from "lucide-svelte";
+  import * as Popover from "@/components/ui/popover/index";
+  import Checkbox from "../ui/checkbox/checkbox.svelte";
+  import Label from "../ui/label/label.svelte";
+  import Separator from "../ui/separator/separator.svelte";
+  import CalendarIcon from "lucide-svelte/icons/calendar";
+  import { writable } from "svelte/store";
+  import {
+    DateFormatter,
+    type DateValue,
+    getLocalTimeZone,
+  } from "@internationalized/date";
+  import Calendar from "../ui/calendar/calendar.svelte";
+  import { cn } from "@/lib/utils";
+  import { toast } from "svelte-sonner";
+
+  const eventTypes = [
+    { type: "person", heading: "Person Detection", color: "#FF4764" },
+    { type: "fire", heading: "Fire Detection", color: "#FF5733" },
+    { type: "face", heading: "Face Detection", color: "#24DA8E" },
+    { type: "heatmap", heading: "Heatmap Detection", color: "#FFB347" },
+    { type: "alpr", heading: "Vehicle Detection", color: "#8A2BE2" },
+  ];
+
+  let startTime: string = "";
+  let endTime: string = "";
+
+  let selectedEventFilters = writable<string[]>([]);
+  const df = new DateFormatter("en-US", {
+    dateStyle: "short",
+  });
+
+  let value: DateValue | undefined = undefined;
+  let isDateOpen = false;
+  $: if (value) {
+    isDateOpen = false;
+  }
+
+  function validateEndTime() {
+    if (endTime && endTime <= startTime) {
+      endTime = "";
+      toast.error("End time must be after start time");
+    }
+  }
+
+  function clearFilters() {
+    endTime = "";
+    startTime = "";
+    value = undefined;
+    selectedEventFilters.set([]);
+  }
 </script>
 
-<div class="w-52 ml-8 mt-3 flex justify-end">
-  <NodeSelection />
+<div class="w-[95%] px-4 ml-8 mt-3 flex justify-between items-center">
+  <div class="w-52">
+    <NodeSelection />
+  </div>
+  <Popover.Root>
+    <Popover.Trigger>
+      <Button variant="outline" class="flex items-center gap-2"
+        ><Filter size={18} />Filters
+      </Button>
+    </Popover.Trigger>
+    <Popover.Content class="mt-2 w-80">
+      <div class="flex w-full items-center justify-between my-2">
+        <h1>Events Filter</h1>
+        <Button variant={"outline"} size={"sm"} on:click={clearFilters}
+          >Clear all</Button
+        >
+      </div>
+      <p>Types</p>
+      <Separator class="my-2" />
+      <div class="grid grid-cols-2 gap-4 w-full">
+        {#each eventTypes as event}
+          <div class="flex items-center justify-between w-1/2 gap-2">
+            <Checkbox
+              checked={$selectedEventFilters.includes(event.type)}
+              onCheckedChange={(v) => {
+                if (v) {
+                  selectedEventFilters.update((value) => {
+                    return [...value, event.type];
+                  });
+                } else {
+                  selectedEventFilters.update((value) => {
+                    return value.filter((val) => val !== event.type);
+                  });
+                }
+              }}
+            />
+            <Label class="text-sm text-nowrap">{event.heading}</Label>
+          </div>
+        {/each}
+      </div>
+      <p class="mt-3">Date</p>
+      <Separator class="my-2" />
+      <div class="flex items-center justify-between w-1/2 gap-2">
+        <Label class="text-sm text-nowrap">Select a date to filter by:</Label>
+        <Popover.Root bind:open={isDateOpen}>
+          <Popover.Trigger asChild let:builder>
+            <Button
+              variant="outline"
+              class={cn(
+                "w-[200px] h-8 justify-start text-left font-normal",
+                !value && "text-muted-foreground"
+              )}
+              builders={[builder]}
+            >
+              <CalendarIcon class="mr-2 h-4 w-4" />
+              {value
+                ? df.format(value.toDate(getLocalTimeZone()))
+                : "Pick a date"}
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content class="w-auto p-0">
+            <Calendar bind:value initialFocus />
+          </Popover.Content>
+        </Popover.Root>
+      </div>
+      <p class="mt-3">Time</p>
+      <Separator class="my-2" />
+      <div class="flex flex-col gap-2">
+        <div class="flex items-center justify-between">
+          <Label class="text-sm text-nowrap">Select a time to filter by:</Label>
+          <Button
+            variant="outline"
+            size="icon"
+            class="size-6 rounded-full"
+            on:click={() => {
+              startTime = "";
+              endTime = "";
+            }}
+          >
+            <X size={12} />
+          </Button>
+        </div>
+        <div class="flex items-center gap-3">
+          <div>
+            <Label>Start Time:</Label>
+            <input
+              type="time"
+              id="startTime"
+              placeholder="00:00"
+              bind:value={startTime}
+              on:input={validateEndTime}
+              class="rounded-md border-2 text-sm px-2 py-2 leading-tight w-full"
+            />
+          </div>
+          <div>
+            <Label>End Time:</Label>
+            <input
+              type="time"
+              id="endTime"
+              placeholder="00:00"
+              bind:value={endTime}
+              on:input={validateEndTime}
+              class="rounded-md border-2 text-sm px-2 py-2 leading-tight w-full"
+            />
+          </div>
+        </div>
+      </div>
+    </Popover.Content>
+  </Popover.Root>
 </div>
 
 <section class="right flex-1 flex h-screen w-full justify-between">
   <div class="w-full h-full px-4">
-    <EventGrid />
+    <EventGrid
+      selectedEventTypeFilters={$selectedEventFilters}
+      date={value}
+      time={{ startTime, endTime }}
+    />
   </div>
-  <!-- 
-  <div
-    class={`relative h-[calc(100vh-75px)] ${showRightPanel ? "w-1/4 max-w-72 " : " w-0"} transition-width ease-in-out duration-500 dark:border-[#292929] border-x-[1px]`}
-  >
-    <button
-      on:click={() => (showRightPanel = !showRightPanel)}
-      class={`absolute -left-6 py-1 rounded-l-md bg-[#f9f9f9] dark:bg-slate-800  top-1/2 -translate-y-1/2 shadow-md transition-position ease-in-out duration-500 z-[99999]`}
-    >
-      <ChevronRight
-        class={`${showRightPanel ? "rotate-0" : "rotate-180"} transition-transform ease-in-out duration-700`}
-      />
-    </button>
-    <div
-      class={`${showRightPanel ? "opacity-100" : "opacity-0"} transtion-opacity ease-in-out duration-500 `}
-    >
-      <div class="relative w-full p-1 bg-[#f9f9f9] dark:bg-[#333333]">
-        <input
-          id="search-input"
-          type="text"
-          class="bg-transparent text-[#020202] font-medium dark:text-white capitalize text-sm sm:text-[8px] md:text-[10px] lg:text-sm xl:text-md rounded-lg block px-10 py-2 box-border dark:focus:border-black dark:active:border-black w-full"
-          placeholder="Enter Date  DD MONTH YY"
-          bind:value={searchDate}
-          on:input={handleDateInput}
-        />
-        <span
-          class="absolute top-1/2 -translate-y-1/2 left-[10px] cursor-pointer z-20"
-        >
-          <LucideCalendarDays class="text-[#727272] h-4 w-4 dark:text-white" />
-        </span>
-        <span
-          class="absolute top-1/2 -translate-y-1/2 right-[10px] cursor-pointer scale-90"
-        >
-          <X class="text-[#727272] h-4 w-4 dark:text-white" />
-        </span>
-        {#if showCalendar}
-          <Calendar
-            bind:value
-            class=" bg-white dark:bg-black absolute top-14 z-40 px-4 py-2 flex flex-col items-center justify-center rounded-md border border-solid border-[#929292]"
-          />
-        {/if}
-      </div>
-      <span
-        class="text-black dark:text-white font-semibold text-base leading-tight w-[90%] mx-auto flex items-center justify-between py-4"
-        >Live Alerts
-        <span class="flex items-center gap-4"
-          ><FilterIcon
-            class="h-6 w-6 p-1 bg-[#f9f9f9] rounded-full text-[#727272] dark:bg-[#333] dark:text-white"
-          />
-          <Settings
-            class="h-6 w-6 p-1 bg-[#f9f9f9] rounded-full text-[#727272] dark:bg-[#333] dark:text-white"
-          /></span
-        >
-      </span>
-      <EventList />
-    </div>
-  </div>
-  <div
-    class="flex flex-col gap-4 items-center justify-center px-2 h-[calc(100vh-76px)]"
-  >
-    <span class="group flex flex-col gap-0.5 items-center justify-center">
-      <button
-        class={`text-black/[.23] h-[40px] w-[40px] rounded-full shadow-md group border-2 border-solid border-black/[.23] dark:border-white/[.23] bg-white dark:bg-black dark:text-white group-hover:text-white group-hover:bg-[#015a62] dark:group-hover:bg-[#258d9d] group-hover:border-none grid place-items-center`}
-        ><Search class="h-[22px] w-[22px]" /></button
-      >
-      <p
-        class="text-xs group-hover:text-[#015a62] dark:group-hover:text-[#258d9d] text-black/[.23] dark:text-white"
-      >
-        Search
-      </p>
-    </span>
-    <span
-      class="group flex flex-col gap-0.5 items-center justify-center relative"
-    >
-      <button
-        on:click={() => (showFilters = !showFilters)}
-        class={!showFilters
-          ? `text-black/[.23] h-[40px] w-[40px] rounded-full shadow-md  border-2 border-solid border-black/[.23] dark:border-white/[.23] bg-white dark:bg-black dark:text-white group-hover:text-white group-hover:bg-[#015a62] dark:group-hover:bg-[#258d9d] group-hover:border-none grid place-items-center `
-          : `relative border-none rounded-full shadow-md h-[40px] w-[40px] text-white bg-[#015a62] grid place-items-center dark:bg-[#258d9d]`}
-        ><Filter class="h-[22px] w-[22px]" />
-      </button>
-      <p
-        class="text-xs group-hover:text-[#015a62] dark:group-hover:text-[#258d9d] text-black/[.23] dark:text-white"
-      >
-        Filter
-      </p>
-      {#if showFilters}
-        <div
-          id="dropdownDefaultCheckbox"
-          class="z-20 flex w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-md dark:bg-[#242424] dark:divide-gray-600 absolute right-14"
-        >
-          <ul
-            class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"
-            aria-labelledby="dropdownCheckboxButton"
-          >
-            <li>
-              <label
-                for="checkbox-item-1"
-                class=" text-sm font-medium text-gray-900 dark:text-gray-100"
-              >
-                <input
-                  id="checkbox-item-1"
-                  type="checkbox"
-                  class="w-4 h-4 mr-2"
-                />
-                Object Detection
-              </label>
-            </li>
-            <li>
-              <label
-                for="checkbox-item-2"
-                class="flex items-center text-sm font-medium text-gray-900 dark:text-gray-100"
-              >
-                <input
-                  id="checkbox-item-2"
-                  type="checkbox"
-                  class="w-4 h-4 mr-2"
-                />
-                Fire Detection
-              </label>
-            </li>
-            <li>
-              <label
-                for="checkbox-item-3"
-                class=" text-sm font-medium text-gray-900 dark:text-gray-100 flex items-center"
-              >
-                <input
-                  id="checkbox-item-3"
-                  type="checkbox"
-                  class="w-4 h-4 mr-2"
-                />
-                24 hour feed
-              </label>
-            </li>
-            <li>
-              <label
-                class=" flex items-center text-sm font-medium text-gray-900 dark:text-gray-100"
-              >
-                <input
-                  id="checkbox-item-4"
-                  type="checkbox"
-                  class="w-4 h-4 mr-2"
-                />
-                Action Detection
-              </label>
-            </li>
-            <li>
-              <label
-                for="checkbox-item-5"
-                class="flex items-center text-sm font-medium text-gray-900 dark:text-gray-100"
-              >
-                <input
-                  id="checkbox-item-5"
-                  type="checkbox"
-                  class="w-4 h-4 mr-2"
-                />
-                All
-              </label>
-            </li>
-          </ul>
-        </div>
-      {/if}
-    </span>
-    <span class="group flex flex-col gap-0.5 items-center justify-center">
-      <button
-        on:click={() => handleEventFullscreen()}
-        class={`text-black/[.23] h-[40px] w-[40px] rounded-full shadow-md group border-2 border-solid border-black/[.23] dark:border-white/[.23] bg-white dark:bg-black dark:text-white group-hover:text-white group-hover:bg-[#015a62] dark:group-hover:bg-[#258d9d] group-hover:border-none grid place-items-center`}
-        ><Expand class="h-[22px] w-[22px]" />
-      </button>
-      <p
-        class="text-xs group-hover:text-[#015a62] dark:group-hover:text-[#258d9d] text-black/[.23] dark:text-white"
-      >
-        Fullscreen
-      </p>
-    </span>
-  </div> -->
 </section>
