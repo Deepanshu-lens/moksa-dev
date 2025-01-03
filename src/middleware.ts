b1b38586ea9b3e0f391ecb0ea0c0ca80f810071e
deepanshu <deepanshu.khorwal@lenscorp.ai>
2025-01-03 18:09:11 +0530

    moksa token changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { defineMiddleware } from "astro:middleware";
 
-let mToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjI0LCJmaXJzdF9uYW1lIjoibWFpbiIsImxhc3RfbmFtZSI6InVzZXIiLCJlbWFpbCI6Im1haW4udXNlckBtb2tzYS5haSIsImlhdCI6MTczNTgyMDk5MiwiZXhwIjoxNzM1OTA3MzkyfQ.AjmlXHRYv-yLf9udUDKA3mOvlHKX02FJuWz9ycQZvTI`;
+let mToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjI0LCJmaXJzdF9uYW1lIjoibWFpbiIsImxhc3RfbmFtZSI6InVzZXIiLCJlbWFpbCI6Im1haW4udXNlckBtb2tzYS5haSIsImlhdCI6MTczNTkwNzg3MCwiZXhwIjoxNzM1OTk0MjcwfQ.7vS5CWpcwtm1-GZx7famXehGeUsRalSYXV9RlSOV7F4`;
 
 export const onRequest = defineMiddleware(async (context, next) => {
   context.locals.moksa = {
