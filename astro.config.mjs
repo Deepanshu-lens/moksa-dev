2186875b0d050c2c2efb209fdf8e01d56db512ac
deepanshu <deepanshu.khorwal@lenscorp.ai>
2025-01-08 18:26:45 +0530

    astro config changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,27 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import dotenv from "dotenv";
-
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
-
 import icon from "astro-icon";
-
-// import node from "@astrojs/node";
-
+import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
-
+import path from "path";
 dotenv.config();
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  // outDir: !process.env.VERCEL ? "../output" : "dist",
-  // trailingSlash: "never",
-  // base: "/",
-
+  // outDir: !process.env.VERCEL ? "output" : "dist",
+  trailingSlash: "never",
+  base: "/",
   // build: {
   //   format: !process.env.VERCEL ? "file" : "directory",
   //   assetsPrefix: "./", // Ensures that asset paths are relative
   // },
-
+  build: {
+    format: !process.env.VERCEL ? "file" : "directory",
+    assetsPrefix: "./",
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -34,8 +29,18 @@ export default defineConfig({
     svelte(),
     icon(),
   ],
-
   devToolbar: { enabled: false },
-
-  adapter: vercel(),
+  ...(process.env.NODE_ENV === "production" && {
+    vite: {
+      base: "./",
+      ssr: {
+        noExternal: true,
+      },
+    },
+  }),
+  adapter: process.env.VERCEL
+    ? vercel()
+    : node({
+        mode: "standalone",
+      }),
 });
